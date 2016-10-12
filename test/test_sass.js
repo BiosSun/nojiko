ecfc4e2d93eb2fc99dd1cd8128ffe162cd66f677
Bios Sun <biossun@gmail.com>
2016-10-13 00:18:19 +0800

    添加单元测试脚本

---
+++ test/test_sass.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var sassTrue = require('sass-true');
+
+var sassFile = path.join(__dirname, 'test.scss');
+
+sassTrue.runSass({
+    file: sassFile,
+    includePaths: ['./node_modules/']
+}, describe, it);
